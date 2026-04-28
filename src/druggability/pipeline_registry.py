23de0e02c985cf6061816de17a8597626129d4a9
miloszbargiel <miloszbargiel@gmail.com>
2026-04-28 19:19:49 +0200

    Add protein comparison pipeline

--- src/druggability/pipeline_registry.py
+++ src/druggability/pipeline_registry.py
@@ -3,6 +3,8 @@
 from __future__ import annotations
 
 from kedro.framework.project import find_pipelines
+from druggability.pipelines.protein_unzip import create_pipeline as create_protein_unzip_pipeline
+from druggability.pipelines.protein_compare import create_pipeline as create_protein_compare_pipeline
 from kedro.pipeline import Pipeline
 
 
@@ -14,4 +16,6 @@ def register_pipelines() -> dict[str, Pipeline]:
     """
     pipelines = find_pipelines(raise_errors=True)
     pipelines["__default__"] = sum(pipelines.values())
+    pipelines["unzip"] = create_protein_unzip_pipeline()
+    pipelines["compare"] = create_protein_compare_pipeline()
     return pipelines
